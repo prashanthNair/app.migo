ab21b6798f5985c7dec09c01a7d71bb1fc616209
rajatdua77 <rajatdua777737@gmail.com>
2021-09-10 00:10:48 +0530

    About link added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { Switch, HashRouter, Route } from "react-router-dom";
 import Home from "./components/home-v2";
+import About from "./components/about";
 
 function App() {
   return (
     <div className="App">
-      <HashRouter basename="/">
-        <Switch>
-          <Route exact path="/" component={Home} />
-        </Switch>
-      </HashRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+      </Switch>
     </div>
   );
 }
