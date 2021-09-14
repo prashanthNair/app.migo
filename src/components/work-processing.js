e4b3979613617e5dfc6ec8dc95f7f85bd4d485b1
rajatdua77 <rajatdua777737@gmail.com>
2021-09-14 21:57:48 +0530

    Slider Added

--- src/components/work-processing.js
+++ src/components/work-processing.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import Navbar from './global-components/navbar-v2';
-import PageHeader from './global-components/page-header';
-import WorkProcessing from './section-components/work-processing';
-import Subscribe from './section-components/subscribe';
-import FooterV2 from './global-components/footer-v2';
+import React from "react";
+import Navbar from "./global-components/navbar-v2";
+import PageHeader from "./global-components/page-header";
+import SliderV2 from "./section-components/slider-v2";
+import WorkProcessing from "./section-components/work-processing";
+import Subscribe from "./section-components/subscribe";
+import FooterV2 from "./global-components/footer-v2";
 
 const WorkProcessingPage = () => {
-    return <div>
-        <Navbar />
-        <PageHeader headertitle="Work Processing" />
-        <WorkProcessing customclass="pd-top-120" />
-        <Subscribe />
-        <FooterV2 />
+  return (
+    <div>
+      <Navbar />
+      <SliderV2 />
+      <WorkProcessing customclass="pd-top-120" />
+      <Subscribe />
+      <FooterV2 />
     </div>
-}
+  );
+};
 
-export default WorkProcessingPage
-
+export default WorkProcessingPage;
